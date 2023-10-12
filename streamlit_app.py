c6fb074103538610cd748301640d9953135a63ce
Jonator3 <joey.pehlke@gmail.com>
2023-10-12 22:21:08 +0200

    add read setup from already existing config.ini

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import os
-
 import streamlit as st
 from apis import hue_api
 from configparser import ConfigParser
@@ -48,7 +47,17 @@ if "wh_pid" not in st.session_state:
     st.session_state.wh_pid = None  # None indicating that WaylandHue is not running
 if "hue_user" not in st.session_state:
     st.session_state.hue_user = None
+if "hue_bridge_ip" not in st.session_state:
+    st.session_state.hue_bridge_ip = "192.168.x.x"
+if "hue_target_group" not in st.session_state:
+    st.session_state.hue_target_group = None
 
+if os.path.exists(base_path+"config.ini"):
+    conf = ConfigParser()
+    conf.read(base_path+"config.ini")
+    if conf["API"]["mode"] == "hue":
+        st.session_state.hue_bridge_ip = conf["API"]["ip"]
+        st.session_state.hue_user = conf["API"]["user"]
 
 st.markdown('# WaylandHue')
 
@@ -58,21 +67,21 @@ if st.session_state.wh_pid is not None:
         st.session_state.wh_pid = None
         st.rerun()
 else:
-    hue_bridge_ip = st.text_input("Bridge IP:", "192.168.x.x")
-    if is_valid_ip(hue_bridge_ip):
+    st.session_state.hue_bridge_ip = st.text_input("Bridge IP:", st.session_state.hue_bridge_ip)
+    if is_valid_ip(st.session_state.hue_bridge_ip):
         st.text("After updating the Bridge IP,\npress the big button on your Hue-Bridge\nand then press the \"Link\" button below.")
         if st.button("Link HUE-Bridge"):
-            st.session_state.hue_user = hue_api.create_new_user(hue_bridge_ip)
+            st.session_state.hue_user = hue_api.create_new_user(st.session_state.hue_bridge_ip)
             if st.session_state.hue_user is None:  #create user failed!
                 st.warning("Failed to link Hue-Bridge, please check the IP and try again.")
         if st.session_state.hue_user is not None:
-            hue_bridge = hue_api.HueBridge(hue_bridge_ip, st.session_state.hue_user)
+            hue_bridge = hue_api.HueBridge(st.session_state.hue_bridge_ip, st.session_state.hue_user)
             groups = hue_bridge.get_groups()
-            target_name = st.selectbox("Light Group to be controlled:", [g.name for g in groups])
-            target_id = [g.id for g in groups if g.name == target_name][0]
+            target_name = st.selectbox("Light Group to be controlled:", [g.name for g in groups], groups.index([g for g in groups if g.id == st.session_state.hue_target_group][0]))
+            st.session_state.hue_target_group = [g.id for g in groups if g.name == target_name][0]
 
-            brightness_factor = st.slider("Brightness factor:", 0.0, 1.0, step=0.01)
+            brightness_factor = st.slider("Brightness factor:", 0.0, 1.0, 1.0, step=0.01)
 
             if st.button("Start WaylandHue Desktop sync"):
-                write_conf(hue_bridge_ip, st.session_state.hue_user, target_id, brightness_factor)
+                write_conf(st.session_state.hue_bridge_ip, st.session_state.hue_user, st.session_state.hue_target_group, brightness_factor)
                 st.session_state.wh_pid = subprocess.Popen(["python3", base_path+"main.py"]).pid
